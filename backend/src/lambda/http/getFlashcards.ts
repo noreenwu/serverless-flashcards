d409dced8d3c4e520fab42b0ab335b176535720a
Noreen Wu <noreenwu@gmail.com>
2021-10-09 19:19:07 -0400

    get a response from the configured lambda function

--- backend/src/lambda/http/getFlashcards.ts
+++ backend/src/lambda/http/getFlashcards.ts
@@ -1,20 +1,21 @@
 import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
-import { getAllFlashcards } from '../../businessLogic/flashcards'
+// import { getAllFlashcards } from '../../businessLogic/flashcards'
 import { getToken } from '../utils'
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
 
     const jwtToken = getToken(event)
-    const flashcards = await getAllFlashcards(jwtToken)
-
+    // const flashcards = await getAllFlashcards(jwtToken)
+    console.log(jwtToken)
     return {
         statusCode: 200,
         headers: {
           'Access-Control-Allow-Origin': '*'
         },
         body: JSON.stringify({
-            items: flashcards
+            message: "hello"
+            // items: flashcards
         })
     }
 }
