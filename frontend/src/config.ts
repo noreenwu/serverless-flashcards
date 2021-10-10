96448cbb911594f6bddfc1b5ec913491ebadd1bd
Noreen Wu <noreenwu@gmail.com>
2021-10-09 21:33:42 -0400

    baseline: to be edited

---
+++ frontend/src/config.ts
@@ -0,0 +1,13 @@
+// The apiId here identifies the API in AWS APIGateway that is being called.
+// The apiEndpoint is the API's baseUrl.
+// const apiId = '40g2xhokvj'
+const apiId = 'n74s9qp5c0'   // flashcards
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev-v11`
+
+export const authConfig = {
+  domain: 'wudev.auth0.com',                             // Auth0 domain
+  // clientId: '1rjeLFNomhhCoa9nEP0SKt5WAGi4sciN',          // Auth0 client id
+  clientId: '1i5eOTEtMWXd0tpFBRiu4vu32oxPhvXd',   // flashcards
+  callbackUrl: 'http://localhost:3000/callback'          // callback url
+}
+
