13d54663652983eee9100948970d2e26a79b1d58
Noreen Wu <noreenwu@gmail.com>
2021-10-09 22:54:50 -0400

    pull any flashcards that exist

--- backend/src/lambda/http/getFlashcards.ts
+++ backend/src/lambda/http/getFlashcards.ts
@@ -1,12 +1,12 @@
 import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
-// import { getAllFlashcards } from '../../businessLogic/flashcards'
+import { getAllFlashcards } from '../../businessLogic/flashcards'
 import { getToken } from '../utils'
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
 
     const jwtToken = getToken(event)
-    // const flashcards = await getAllFlashcards(jwtToken)
+    const flashcards = await getAllFlashcards(jwtToken)
     console.log(jwtToken)
     return {
         statusCode: 200,
@@ -14,8 +14,8 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
           'Access-Control-Allow-Origin': '*'
         },
         body: JSON.stringify({
-            message: "hello"
-            // items: flashcards
+            message: "hello",
+            items: flashcards
         })
     }
 }
