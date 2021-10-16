4b7f43afad12d1bd3c11c35aba6e80e28258e539
Noreen Wu <noreenwu@gmail.com>
2021-10-16 18:09:55 -0400

    initial commit

---
+++ backend/src/lambda/http/getFlashcardsByCategory.ts
@@ -0,0 +1,22 @@
+import 'source-map-support/register'
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { getAllFlashcardsByCategory } from '../../businessLogic/flashcards'
+import { getToken } from '../utils'
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const category = event.pathParameters.category
+    console.log("category", category)
+    const jwtToken = getToken(event)
+    const flashcards = await getAllFlashcardsByCategory(jwtToken, category)
+    console.log(jwtToken)
+    return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify({
+            message: "get flashcards by category",
+            items: flashcards
+        })
+    }
+}
