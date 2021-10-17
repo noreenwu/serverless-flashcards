511f794958af5d00a173747a305d3945f18c9e7d
Noreen Wu <noreenwu@gmail.com>
2021-10-16 22:08:18 -0400

    return 400 if both parameters are missing or category parameter is missing

--- backend/src/lambda/http/getFlashcardsByCategory.ts
+++ backend/src/lambda/http/getFlashcardsByCategory.ts
@@ -3,12 +3,33 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } f
 import { getAllFlashcardsByCategory } from '../../businessLogic/flashcards'
 import { getToken } from '../utils'
 
+const return400 = (message: string) => {
+  return {
+    statusCode: 400,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+        message: message,
+    })
+  }  
+}
+
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     // const category = event.pathParameters.category
+
+    if ( event.queryStringParameters == null ) {
+      return return400("get flashcards by category: no parameters specified")
+    }
+    if ( event.queryStringParameters['category'] == null ) {
+      return return400("get flashcards by category: no category specified")
+    }
+
     const category = event.queryStringParameters['category']
     const mastery = event.queryStringParameters['mastery']
     console.log("mastery specified ", mastery)
     const jwtToken = getToken(event)
+
     const flashcards = await getAllFlashcardsByCategory(jwtToken, category, mastery)
     console.log(jwtToken)
     return {
